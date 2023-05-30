bb82dd6e097240626cf4ad591c372dff23faa233
Dou, Chao <chao.dou@intel.com>
2023-05-30 17:27:09 +0800

    two docker for building ngix runtime image

---
+++ nginx-quic-docker/clone_source.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+git clone https://github.com/google/boringssl 
+hg clone -b quic https://hg.nginx.org/nginx-quic
---
+++ nginx-quic-docker/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+nginx -p /root -c /root/nginx_etc/conf/nginx_ref2.conf
